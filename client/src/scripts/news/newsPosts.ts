9b015022b5e8250238696f48d6e3739a2a3a61bb
blazingyoda <146597954+Ukrainian-dude@users.noreply.github.com>
2023-09-30 22:06:24 -0700

    e

---
+++ client/src/scripts/news/newsPosts.ts
@@ -0,0 +1,50 @@
+/* eslint-disable quotes,@typescript-eslint/quotes */
+import { type NewsPost } from "./newsHelper";
+
+export const news: NewsPost[] = [
+
+ {
+        date: 1694706686000,
+        author: "Ukraines dude",
+        title: "Return to form",
+        bannerImage: "./img/news/v0.11.0.png",
+        content: `Yo yo whats good bros, I dont care much to do a lore post here, reason 1 being canonically the things added in 2.0 were always here, and reason 2 idk the lore lol, but anyways, new stuff! 2 new POI's and 2 new guns!<br><br>
+
+Also smaller island lol<br><br>
+
+And finally, the flint stone drops loot now :) (30X IS NOT CLICKBAIT FINALLY)`,
+        longContent: `<h2>re-savannah mode v0.1.0</h2>
+
+<h3>New features & changes</h3>
+<ul>
+  <li>Check the changelog you nerd </li>
+</ul>
+
+Special thanks (once again) to Ukraines dude and Jason, we made this thing lol`
+    },
+ 
+ {
+        date: 1694706686000,
+        author: "H.A.Z.E.L",
+        title: "Broadening our horizons",
+        bannerImage: "./img/news/v0.10.0.png",
+        content: `We here at H.A.Z.E.L have broadened our horizons, and expanded operations to a new island! DMR's and snipers reign supreme in the savannah! But settlement operations have been delayed... indefinitely. Which leads to houses being an impossible find here...<br><br>
+
+We've also began experimenting with a new, lighter form of transporting oil, the propane tank! These tiny barrels are convenient for transporting oil due to the scarcity of oil on this island. But their smaller size means they could theoretically be exploded much easier than regular barrels...<br><br>
+
+*End transmission*<br><br>
+"We have some bad news. During transportation the flint crate was... damaged to say the least. But I know the combatants won't assume anything out of the ordinary.."`,
+        longContent: `<h2>re-savannah mode v0.1.0</h2>
+
+<h3>New features & changes</h3>
+<ul>
+  <li>Surviv.io's iconic savannah mode is back in all of it's glory! This includes: </li>
+  <li>No More houses.</li>
+  <li>The return of the SVD-63! (DMR). </li>
+  <li>Less tree's, more rocks. </li>
+  <li>Surviv.io savannah mode weapons only. </li>
+</ul>
+
+Special thanks to Ukraines dude and Jason, we made this thing lol`
+    },
+];
